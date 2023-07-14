37d3c8c2b318959e802e4504bfba1c9cde309f5a
SamuelMeline <melsam0905@gmail.com>
2023-07-14 18:13:35 +0200

    create navbar + add css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
+    #[Route('/', 'home.index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('home.html.twig');
